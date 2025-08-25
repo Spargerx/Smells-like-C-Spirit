e319fd55db02213658a7e4245ece6fe8b2318ee0
Sparger <mahiyad2005@gmail.com>
2025-08-26 00:53:55 +0530

    printing output

---
+++ Day_1/printing_output.c
@@ -0,0 +1,10 @@
+#include <stdio.h>  // preprocessors
+
+// main function of c ... Starting point of every C programs
+int main() {
+
+    printf("Hello, World!\n"); // '\n' is an escape character it moves cursor to next line
+    printf("and Hello, C. It's nice to finally meet you.\n");
+
+    return 0; //return the value 0 to the system so that it can understand that program has ran successfully...
+}
