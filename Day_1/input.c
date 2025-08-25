c7a2c006b07ee1e42098ae490caab789c74cae17
Sparger <mahiyad2005@gmail.com>
2025-08-26 01:08:54 +0530

    asking your name

---
+++ Day_1/input.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    char name[20];
+
+    printf("Kimi No Na Wa ?\n");
+    scanf("%s", &name);
+
+    printf("%s, have we ever met before ?..");
+    return 0;
+}
